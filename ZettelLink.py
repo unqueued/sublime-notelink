5d92e1ce1c8561f48bd2ef5e4d50cac24c82c8cc
xHN35RQ <xHN35RQ@users.noreply.github.com>
2015-07-20 17:14:07 -0700

    Update and rename MyWiki.py to ZettelLink.py

--- ZettelLink.py
+++ ZettelLink.py
@@ -1,13 +1,15 @@
-import sublime, sublime_plugin, os, re, subprocess
+import sublime
+import sublime_plugin
+import os
+import re
+import subprocess
 
-
-
-class FollowWikiLinkCommand(sublime_plugin.TextCommand):
+class FollowZettelLinkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        settings = sublime.load_settings('MyWiki.sublime-settings')
-        directory = settings.get('wiki_directory')
+        settings = sublime.load_settings('ZettelLink.sublime-settings')
+        directory = settings.get('zettel_directory')
         directory = os.path.expanduser(directory)
-        extension = settings.get('wiki_extension')
+        extension = settings.get('zettel_extension')
         window = self.view.window()
 
         oldLocation = self.view.sel()[0]
@@ -17,10 +19,10 @@ class FollowWikiLinkCommand(sublime_plugin.TextCommand):
         self.view.sel().clear()
         self.view.sel().add(oldLocation)
 
-        the_file = directory+selected_text+extension
+        the_file_path = directory+selected_text+extension
+        the_file = selected_text+extension
 
-
-        if os.path.exists(the_file):
+        if os.path.exists(the_file_path):
             #open the already-created page.
             new_view = window.open_file(the_file)
 
@@ -29,36 +31,28 @@ class FollowWikiLinkCommand(sublime_plugin.TextCommand):
             open(the_file, "a")
             new_view = window.open_file(the_file)
 
-       
-
-
-
-
-class GetWikiLinkCommand(sublime_plugin.TextCommand):
+class GetZettelLinkCommand(sublime_plugin.TextCommand):
     def on_done(self, selection):
         if selection == -1:
             self.view.run_command(
-                "insert_wiki_link", {"args":
+                "insert_zettel_link", {"args":
                 {'text': '[['}})
             return
         self.view.run_command(
-            "insert_wiki_link", {"args":
+            "insert_zettel_link", {"args":
             {'text': '[['+self.modified_files[selection]+']]'}})
 
     def run(self, edit):
-        settings = sublime.load_settings('MyWiki.sublime-settings')
-        directory = settings.get('wiki_directory')
+        settings = sublime.load_settings('ZettelLink.sublime-settings')
+        directory = settings.get('zettel_directory')
         directory = os.path.expanduser(directory)
-        extension = settings.get('wiki_extension')
+        extension = settings.get('zettel_extension')
 
         self.outputText = '[['
         self.files = os.listdir(directory)
         self.modified_files = [item.replace(extension,"") for item in self.files]
         self.view.window().show_quick_panel(self.modified_files, self.on_done)
 
-
-
-class InsertWikiLinkCommand(sublime_plugin.TextCommand):
+class InsertZettelLinkCommand(sublime_plugin.TextCommand):
     def run(self, edit, args):
         self.view.insert(edit, self.view.sel()[0].begin(), args['text'])
-
