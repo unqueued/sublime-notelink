8912a171e40c2159e2280639b1f72beac038cf02
Nick <xHN35RQ@users.noreply.github.com>
2015-11-11 07:30:12 -0800

    Update and rename ZettelLink.py to sublime-notelink.py

--- sublime-notelink.py
+++ sublime-notelink.py
@@ -4,12 +4,12 @@ import os
 import re
 import subprocess
 
-class FollowZettelLinkCommand(sublime_plugin.TextCommand):
+class FollowNoteLinkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        settings = sublime.load_settings('ZettelLink.sublime-settings')
-        directory = settings.get('zettel_directory')
+        settings = sublime.load_settings('sublime-notelink.sublime-settings')
+        directory = settings.get('note_directory')
         directory = os.path.expanduser(directory)
-        extension = settings.get('zettel_extension')
+        extension = settings.get('note_extension')
         window = self.view.window()
 
         oldLocation = self.view.sel()[0]
@@ -30,28 +30,28 @@ class FollowZettelLinkCommand(sublime_plugin.TextCommand):
             open(the_file, "a")
             new_view = window.open_file(the_file)
 
-class GetZettelLinkCommand(sublime_plugin.TextCommand):
+class GetNoteLinkCommand(sublime_plugin.TextCommand):
     def on_done(self, selection):
         if selection == -1:
             self.view.run_command(
-                "insert_zettel_link", {"args":
+                "insert_note_link", {"args":
                 {'text': '[['}})
             return
         self.view.run_command(
-            "insert_zettel_link", {"args":
+            "insert_note_link", {"args":
             {'text': '[['+self.modified_files[selection]+']]'}})
 
     def run(self, edit):
-        settings = sublime.load_settings('ZettelLink.sublime-settings')
-        directory = settings.get('zettel_directory')
+        settings = sublime.load_settings('sublime-notelink.sublime-settings')
+        directory = settings.get('note_directory')
         directory = os.path.expanduser(directory)
-        extension = settings.get('zettel_extension')
+        extension = settings.get('note_extension')
 
         self.outputText = '[['
         self.files = os.listdir(directory)
         self.modified_files = [item.replace(extension,"") for item in self.files]
         self.view.window().show_quick_panel(self.modified_files, self.on_done)
 
-class InsertZettelLinkCommand(sublime_plugin.TextCommand):
+class InsertNoteLinkCommand(sublime_plugin.TextCommand):
     def run(self, edit, args):
         self.view.insert(edit, self.view.sel()[0].begin(), args['text'])
